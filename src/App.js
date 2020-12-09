dbb69571fac371f2f96397473110509cd8e0321b
Benjamin Amoah <benjaminamoah@Benjamins-MBP.home>
2020-12-09 15:04:50 -0500

    21st commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Deployment from GitHub to S3
+          Deployment from GitHub to S3 v.1.0
         </p>
       </header>
     </div>
